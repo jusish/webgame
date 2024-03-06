5ac4c707826dc49b10485dc48e567f5b23a0395c
jusish <ishimwejustin67@gmail.com>
2024-03-06 21:27:52 +0200

    added trending section

---
+++ src/components/Trending.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import GameCard from "./GameCard";
+
+const Trending = () => {
+  return (
+    <Box margin={15}>
+      <Box>
+        <GameCard />
+      </Box>
+    </Box>
+  );
+};
+
+export default Trending;
