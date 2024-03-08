7b8647abc47ff84a64874f98950d84aaaf483b51
jusish <ishimwejustin67@gmail.com>
2024-03-08 11:22:06 +0200

    Created new component for section

---
+++ src/components/Games.tsx
@@ -0,0 +1,110 @@
+import { Avatar, Box, Typography } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const Games = () => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      gap={2}
+      marginTop={10}
+      marginX={20}
+    >
+      <Typography variant="h5" color="white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </Typography>
+      <Typography variant="body1" color="white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,
+      </Typography>
+      <Box
+        marginY={5}
+        display={"flex"}
+        flexDirection={"row"}
+        gap={10}
+        flexWrap={"wrap"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={2}
+        >
+          <Avatar alt="Mobile" src="/public/Group 48.png" />
+          <Typography variant="body1" color="white">
+            Mobile Game Development
+          </Typography>
+          <ArrowForwardIcon sx={{ fill: "#FA8305" }} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Games;
