d2cb3e7e5174a6a2ab3ff85c6eb7601ea210129d
jusish <ishimwejustin67@gmail.com>
2024-03-08 10:01:05 +0200

    trending section changed

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -3,9 +3,12 @@ import GameCard from "./GameCard";
 
 const Trending = () => {
   return (
-    <Box margin={15}>
-      <Box>
-        <GameCard />
+    <Box marginLeft={20} marginY={5}>
+      <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"} gap={10}>
+        <GameCard image="/public/Rectangle 23.png" numberOfFollowers={20} />
+        <GameCard image="/public/Rectangle 24.png" numberOfFollowers={40} />
+        <GameCard image="/public/Rectangle 26.png" numberOfFollowers={25} />
+        <GameCard image="/public/Rectangle 25.png" numberOfFollowers={10} />
       </Box>
     </Box>
   );
