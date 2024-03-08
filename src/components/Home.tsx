556c860ed01d8347862b19ac4115a9106dadd633
jusish <ishimwejustin67@gmail.com>
2024-03-08 14:41:35 +0200

    Home page modified

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,8 @@ import Hero from "./Hero";
 import Lorem from "./Lorem";
 import Image from "./Image";
 import Games from "./Games";
+import RecentProjects from "./RecentProjects";
+import StayInLoop from "./StayInLoop";
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Lorem />
       <Image />
       <Games />
+      <RecentProjects />
+      <StayInLoop />
     </Box>
   );
 };
