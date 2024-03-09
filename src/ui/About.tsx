e55a9898edab4b783b7185a345d7a8fe94f7da07
jusish <ishimwejustin67@gmail.com>
2024-03-09 21:14:43 +0200

    About page created

---
+++ src/ui/About.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import HeroAbout from "../components/HeroAbout";
+import CardList from "../components/CardList";
+
+const About = () => {
+  return (
+    <Box>
+      <HeroAbout />
+      <CardList />
+    </Box>
+  );
+};
+
+export default About;
