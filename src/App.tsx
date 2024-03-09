ebffb68ff8a1c4e622fe88ada05e4d606e235f9c
jusish <ishimwejustin67@gmail.com>
2024-03-09 21:13:40 +0200

    Added app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Home from "./components/Home";
+import Home from "./ui/Home";
 import Navbar from "./components/Navbar";
+import About from "./ui/About";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Portifolio from "./ui/Portifolio";
+import Contact from "./ui/Contact";
+import News from "./ui/News";
 
 const darkTheme = createTheme({
   palette: {
@@ -14,9 +20,17 @@ function App() {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Navbar />
-        <Home />
+        <Router>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" Component={Home}></Route>
+            <Route path="/about" Component={About}></Route>
+            <Route path="/portifolio" Component={Portifolio}></Route>
+            <Route path="/contact" Component={Contact}></Route>
+            <Route path="/news" Component={News}></Route>
+          </Routes>
+        </Router>
       </ThemeProvider>
     </>
   );
