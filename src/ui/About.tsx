42a794d77219b4be47cd32bca78eb47dadd12dd0
jusish <ishimwejustin67@gmail.com>
2024-03-10 21:31:45 +0200

    About section modified

--- src/ui/About.tsx
+++ src/ui/About.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import HeroAbout from "../components/HeroAbout";
 import CardList from "../components/CardList";
+import ImageSection from "../components/ImageSection";
 
 const About = () => {
   return (
-    <Box>
+    <Box display={"flex"} flexDirection={"column"} gap={5}>
       <HeroAbout />
       <CardList />
+      <ImageSection />
     </Box>
   );
 };
